0eb5a3dfc08a1a05fb2206b7086fd78495f882ca
Sebastian Rivera <sebastian-010198@hotmail.com>
2021-04-07 22:17:38 -0500

    Create Icon, Header and navbar components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "../Icon";
+
+const index: React.FC = () => {
+    return (
+        <div>
+            <Icon />
+            Componente Header
+        </div>
+    );
+};
+
+export default index;
---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from "../../assets/icon-header.png";
+
+const index: React.FC = () => {
+    return (
+        <a href="/">
+            <img src={Icon} alt="icono del header" />
+        </a>
+    );
+};
+
+export default index;
